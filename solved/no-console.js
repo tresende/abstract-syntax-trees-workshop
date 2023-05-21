111306f398b6f4af3523133c7b2f351e46481d24
Thiago Resende <thiago.gcresende@gmail.com>
2023-05-21 20:30:59 -0300

    making a babel plugin

---
+++ solved/no-console.js
@@ -0,0 +1,22 @@
+// eslint exercise 0 (no-console)
+// When you're finished with this exercise, run
+//   "npm start exercise.eslint.1"
+//   to move on to the next exercise
+
+module.exports = {
+  // you're going to need context :)
+  // eslint-disable-next-line no-unused-vars
+  create(context) {
+    return {
+      Identifier(node) {
+        if (node.name !== 'console') {
+          return
+        }
+        context.report({
+          node,
+          message: 'Using console is not allowed',
+        })
+      },
+    }
+  },
+}
